add9a20fb6592a5ad54b4abe43ce6282e7c74826
Jung Seung Min <jsm1592@gmail.com>
2024-05-31 22:30:49 +0900

    Slot

---
+++ src/components/CheckBox1.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <input type="checkbox" :value="id" :checked="checked" @change="">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CheckBox1",
+    props: 
+}
+</script>
