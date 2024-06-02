8fc43f88e8f5003180508efac88f1b55272e1305
Jung Seung Min <jsm1592@gmail.com>
2024-06-02 09:52:42 +0900

    slot-test

--- src/components/CheckBox1.vue
+++ src/components/CheckBox1.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
-        <input type="checkbox" :value="id" :checked="checked" @change="">
-
+        <input type="checkbox" :value="id" :checked="checked" @change="$emit('check-changed', 
+            {id, checked: $event.target.checked})" style="color:blue; text-decoration: underline;">
+        <span v-if="checked===true" style="color:blue; text-decoration:underline;">
+            <i>{{ label }}</i>
+        </span>
+        <span v-else style="color:gray">{{label}}</span>
     </div>
 </template>
 
 <script>
 export default {
     name: "CheckBox1",
-    props: 
+    props: ['id', 'checked', 'label']
 }
 </script>
